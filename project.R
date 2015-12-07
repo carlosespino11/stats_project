2ffdd50906a987ae9d4776dda17eac255a23977b
xavierign <xavierign@gmail.com>
2015-12-07 11:05:49 -0500

    Included Mail from Motta removed the crap

--- project.R
+++ project.R
@@ -15,6 +15,16 @@ cancellations_month = flights %>% group_by(Month, UniqueCarrier) %>%
 ggplot(cancellations_month) + geom_line(aes(x=Month, y = sum_canc, color = UniqueCarrier)) + 
   scale_x_continuous(breaks=1:12)
 
-#HOLAAAAAA!!! viva el HUB! 
-#suckers
-###cododskdks
+#chapters from the book:
+#The final project is due on December 15 before 7:40pm. 
+#Please e-mail your project directly to the TA.
+#Your paper should cover at least one application in R from each of the following 3 chapters: 
+#9 & 10 from IMS, and 7 from ISL.
+#You can definitely apply other methods as well (logistic regression, bootstrap, etc).
+#If you have questions about R, please feel free to e-mail the TA or attend his office hours. 
+
+
+
+Number pages: min 15 max 20. Format for submission: pdf or word. 
+
+Number of students per group: min 3 max 4. E-mail (as soon as possible) me if you are not yet part of a team.
