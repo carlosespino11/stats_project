8c5823a86fbf8231d6730d04a8c17775766dd816
Carlos Espino <carlosespino11@gmail.com>
2015-12-07 11:21:04 -0500

    Added data.table to read large dataset

--- project.R
+++ project.R
@@ -1,16 +1,17 @@
 library(dplyr)
 library(ggplot2)
+library(data.table)
 
-setwd("~/Downloads/")
+setwd("~/Documents/Columbia/Statistical Inference and Modeling/stats_project/")
 
-flights <- read.csv("2008.csv") %>% tbl_df()
+flights <-fread("2008.csv") %>% tbl_df()
 
 flights
 
 glimpse(flights)
 
 cancellations_month = flights %>% group_by(Month, UniqueCarrier) %>% 
-  summarize(sum_canc = sum(Cancelled)) %>% arrange( desc(UniqueCarrier)) %>% 
+  summarize(sum_canc = sum(Cancelled)) %>% arrange( desc(UniqueCarrier))
 
 ggplot(cancellations_month) + geom_line(aes(x=Month, y = sum_canc, color = UniqueCarrier)) + 
   scale_x_continuous(breaks=1:12)
